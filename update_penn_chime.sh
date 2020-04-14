deab00f212c1cad153d5f7fbbacaa993775fba5b
misken <isken@oakland.edu>
2020-04-14 14:17:51 -0400

    progress on nhpp model

--- update_penn_chime.sh
+++ update_penn_chime.sh
@@ -5,10 +5,16 @@ cd ./chime
 #conda activate chime
 if [ "$CONDA_PREFIX" == "/home/$USER/anaconda3/envs/chime" ]
 then
+# Sync chime
   git fetch upstream
   git checkout develop
   git merge upstream/develop
   pip install . -q
+# Sync chime_sims
+  cd ../chime_sims
+  git fetch upstream
+  git checkout master
+  git merge upstream/master
 else
   echo 'need to activate conda chime environment'
 fi
